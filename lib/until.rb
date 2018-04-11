28fb38d603367777efc7cdb4471bdad662520a8e
Heidi Engstrom <hengstrom25@gmail.com>
2018-04-11 20:55:48 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,9 +1,8 @@
 def using_until
-       counter = 6
-       until counter = 10
+    counter = 6
+       until counter == 10
        puts "Wingardium Leviosa"
        counter += 1
      end
-    
-end
+  end
 
